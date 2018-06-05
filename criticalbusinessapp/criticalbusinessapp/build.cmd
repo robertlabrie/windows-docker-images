66a10af595f1a1d948b166bea229e4fc29df9e9e
Administrator@lab.local <Administrator@lab.local>
2018-06-05 16:49:35 -0700

    initial

---
+++ criticalbusinessapp/criticalbusinessapp/build.cmd
@@ -0,0 +1,9 @@
+del C:\images\criticalbusinessapp\criticalbusinessapp\criticalbusinessapp\bin\Debug\criticalbusinessapp.exe
+del C:\images\criticalbusinessapp\criticalbusinessapp\criticalbusinessapp\bin\x64\Release\criticalbusinessapp.exe
+del C:\images\criticalbusinessapp\criticalbusinessapp\criticalbusinessapp\bin\x64\Release\criticalbusinessapp.vshost.exe
+del C:\images\criticalbusinessapp\criticalbusinessapp\criticalbusinessapp\obj\x64\Debug\criticalbusinessapp.exe
+del C:\images\criticalbusinessapp\criticalbusinessapp\criticalbusinessapp\obj\x64\Release\criticalbusinessapp.exe
+del C:\images\criticalbusinessapp\criticalbusinessapp\criticalbusinessapp\bin\Release\criticalbusinessapp.exe
+del C:\images\criticalbusinessapp\criticalbusinessapp\criticalbusinessapp\obj\Release\criticalbusinessapp.exe
+
+"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" /p:Configuration=Release;Platform=x64
---
+++ criticalbusinessapp/criticalbusinessapp/criticalbusinessapp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Win32;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace criticalbusinessapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "doing critical business things";
+            try
+            {
+                using (RegistryKey key = Registry.LocalMachine.OpenSubKey("SOFTWARE\\CriticalBusinessProcess"))
+                {
+                    if (key != null)
+                   { 
+                        
+                        Object o = key.GetValue("message");
+                        if (o != null)
+                        {
+                            message = o.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)  
+            {
+                Console.WriteLine("Exception reading registry {0}", ex.ToString());
+                Environment.Exit(1);
+            }
+            for (;;)
+            {
+
+                String timeStamp = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+                Console.WriteLine("{0} {1}", timeStamp, message);
+                System.Threading.Thread.Sleep(5000);
+            }
+        }
+    }
+}
